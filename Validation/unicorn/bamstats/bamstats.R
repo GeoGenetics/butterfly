3d5efbd75f4b79e34afafe1704fb8eda6fd3f257
Julian Regalado <julian.perez@sund.ku.dk>
2025-07-14 14:15:04 +0200

    Added per bamfile mean NM comparison

--- Validation/unicorn/bamstats/bamstats.R
+++
@@ -1,8 +0,0 @@
-args <- commandArgs(trailingOnly = TRUE)
-
-#datafram1
-bamstat2 <- read.table(args[1], header=T, comment.char = "")
-#dataframe2
-bamstat2 <- read.table(args[2], header=T, comment.char = "")
-#metadata
-samplemd <- read.table("../sample_data.txt", header=T)
